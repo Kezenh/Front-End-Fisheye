6d909b61843669b1980a3ea26286f1331a480eed
Quentin Rampteau <quentin425@hotmail.fr>
2021-12-07 16:09:30 +0100

    Data importation

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,7 +1,7 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, tagline, price } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+    const picture = `assets/photographers/id_photos/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
@@ -9,8 +9,17 @@ function photographerFactory(data) {
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        const location = document.createElement('p');
+        location.textContent = city + ", " + country;
+        const tag = document.createElement('p');
+        tag.textContent = tagline;
+        const cost = document.createElement('p');
+        cost.textContent = price + "€/jour";
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(location);
+        article.appendChild(tag);
+        article.appendChild(cost);
         return (article);
     }
     return { name, picture, getUserCardDOM }
