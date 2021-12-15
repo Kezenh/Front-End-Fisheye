4e57c6b3bb60e37ebfa0a4940259d6d879696ced
Quentin Rampteau <quentin425@hotmail.fr>
2021-12-15 13:38:01 +0100

    Home page's integration

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -4,23 +4,44 @@ function photographerFactory(data) {
     const picture = `assets/photographers/id_photos/${portrait}`;
 
     function getUserCardDOM() {
+        //DOM elements
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
         const location = document.createElement('p');
-        location.textContent = city + ", " + country;
         const tag = document.createElement('p');
-        tag.textContent = tagline;
         const cost = document.createElement('p');
+        const linkBox = document.createElement('a');
+        const description = document.createElement('div');
+        
+        //Adding classes
+        location.classList.add("location");
+        tag.classList.add("tag");
+        cost.classList.add("cost");
+
+        //Adding attributes
+        description.setAttribute("aria-label", name + "description");
+        linkBox.setAttribute("href", "");
+        linkBox.setAttribute("aria-label", name);
+        img.setAttribute("src", picture);
+        description.setAttribute("tabindex", "0");
+
+        //Adding content
+        h2.textContent = name;
+        location.textContent = city + ", " + country;
+        tag.textContent = tagline;
         cost.textContent = price + "€/jour";
-        article.appendChild(img);
-        article.appendChild(h2);
-        article.appendChild(location);
-        article.appendChild(tag);
-        article.appendChild(cost);
+
+        //Adding nodes
+        description.appendChild(location);
+        description.appendChild(tag);
+        description.appendChild(cost);
+        linkBox.appendChild(img);
+        linkBox.appendChild(h2);
+        article.appendChild(linkBox);
+        article.appendChild(description);
+        
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return { name, picture, getUserCardDOM };
 }
